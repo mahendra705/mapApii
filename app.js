45e6d886d964873a64ba6f7f654893da90d5a1d0
Manish Agrawal <manishagrawal705@gmail.com>
2017-01-04 20:45:02 +0530

    code change

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
- 
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
- 
+
 var routes = require("./routes/routes.js")(app);
 
- var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+  ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
-
-var server = app.listen(server_port, server_ip_address, function () {
-  console.log( "Listening on " + server_ip_address + ", port " + server_port )
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.status(500).send('Something bad happened!');
 });
+  app.listen(port, ip);
+console.log('Server running on http://%s:%s', ip, port);
